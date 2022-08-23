7005e38a4ede51fbb62c3c2eebc1ca4838f3b379
Rey Halsall <reyeduardohalsallquintero8@gmail.com>
2022-08-23 15:28:11 -0600

    Uploading SQLQuery3 - Data manipulation

---
+++ SQLQuery3 - Manipulacion de datos.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE Practica
+GO
+USE Practica
+
+CREATE TABLE Persona(
+	Nombre VARCHAR(80),
+	Apellido VARCHAR(80),
+	Dni INT)
+
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Martin', 'Marquesi', 26125988)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Pablo', 'Marquesi', 25485699)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Roberto', 'Sanchez', 20566401)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Estefania', 'Guissini', 27128064)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Ruben', 'Alegrato', 24238975)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Sandra', 'Britte', 25483669)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Melisa', 'Ardul', 29456224)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Soledad', 'Michelli', 29889656)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Betania', 'Musachegui', 27128765)
+INSERT INTO Persona(Nombre, Apellido, Dni) VALUES ('Juan', 'Serrat', 28978845)
